2338ef501ac205f0577694cdbeb5a2f4cc50546c
ronaldodeac <80225698+ronaldodeac@users.noreply.github.com>
2021-03-07 23:03:07 -0300

    Update Scripts Iniciais.sql

--- database/Scripts Iniciais.sql
+++ database/Scripts Iniciais.sql
@@ -2,9 +2,9 @@ create database SOA;
 use soa;
 show tables;
 
-create table dispositivo (id int auto_increment primary key not null, descricao varchar(45), status_dispositivo varchar(20), criado_em datetime, token varchar(100));
+create table dispositivo (id int auto_increment primary key not null, descricao varchar(45), status_dispositivo varchar(20), criado_em datetime);
 create table logs_acesso (id int auto_increment primary key not null, on_em datetime, off_em datetime, id_dispositivo int not null, id_usuario int not null, foreign key(id_dispositivo) references dispositivo(id), foreign key(id_usuario) references usuario(id));
-create table usuario (id int auto_increment primary key not null, nome varchar(45));
+create table usuario (id int auto_increment primary key not null, nome varchar(45), token varchar(100));
 
 
 select * from dispositivo;
